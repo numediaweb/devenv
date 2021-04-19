9ebd267f9a80b0d45b28bb02e0142ac9eafbe7a7
Abdessamad Idrissi <numediaweb@gmail.com>
2021-04-19 11:26:03 +0200

    ignore errors due to php and zsh

--- ansible/roles/apache/files/bin/phpswitch.sh
+++ ansible/roles/apache/files/bin/phpswitch.sh
@@ -8,9 +8,10 @@ echo "The current versioin is: $VERSION"
 sudo a2dismod php5.6 php7.4 php8.0 &&
 sudo a2enmod php"$VERSION" &&
 sudo service apache2 restart &&
+sudo systemctl restart apache2 &&
 
 sudo update-alternatives --set php /usr/bin/php"$VERSION" &&
 sudo update-alternatives --set php-config /usr/bin/php-config"$VERSION" &&
 sudo update-alternatives --set phpize /usr/bin/phpize"$VERSION" &&
-sudo update-alternatives --set phar /usr/bin/php"$VERSION" &&
+#sudo update-alternatives --set phar /usr/bin/php"$VERSION" &&
 echo "Successfully switched to $VERSION"
